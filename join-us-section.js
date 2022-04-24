526893b2408dfbe30b7c2f1e41f2381da0018109
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-04-24 14:53:05 +0400

    wrote single-quotes for strings. eslint command - quotes

--- join-us-section.js
+++ join-us-section.js
@@ -8,19 +8,19 @@ class Section {
     this.btnText = btnText;
     if (!instance) {
       window.addEventListener('load', () => {
-        const main = document.getElementById("app-container");
-        const footer = document.querySelector(".app-footer");
+        const main = document.getElementById('app-container');
+        const footer = document.querySelector('.app-footer');
         const section = document.createElement('section');
         const div = document.createElement('div');
         const h2 = document.createElement('h2');
         h2.textContent = headline;
         const p = document.createElement('div');
         const str1 = document.createElement('p');
-        str1.textContent = "Sed do eiusmod tempor incididunt";
+        str1.textContent = 'Sed do eiusmod tempor incididunt';
         const str2 = document.createElement('p');
-        str2.textContent = "ut labore et dolore magna aliqua.";
-        const emailBox = document.createElement("form");
-        const emailInput = document.createElement("input");
+        str2.textContent = 'ut labore et dolore magna aliqua.';
+        const emailBox = document.createElement('form');
+        const emailInput = document.createElement('input');
         emailInput.setAttribute('type', 'email');
         emailInput.setAttribute('placeholder', 'Email');
         const emailBtn = document.createElement('button');
@@ -36,13 +36,13 @@ class Section {
         emailBox.appendChild(emailInput);
         emailBox.appendChild(emailBtn);
 
-        section.classList.add("added-section");
+        section.classList.add('added-section');
         div.classList.add('added-section__content');
         h2.classList.add('added-section__h2');
         p.classList.add('added-section__p');
         str1.classList.add('added-section__p-string');
         str2.classList.add('added-section__p-string');
-        emailBox.classList.add("added-section__email-box");
+        emailBox.classList.add('added-section__email-box');
         emailInput.classList.add('added-section__email-input');
         emailBtn.classList.add('added-section__email-btn');
 
@@ -60,7 +60,7 @@ export default class SectionCreator {
   create(type) {
     switch(type) {
       case 'standard':
-        return new Section ("Join Our Program", "SUBSCRIBE")
+        return new Section ('Join Our Program', 'SUBSCRIBE')
       case 'advanced':
         return new Section ('Join Our Advanced Program', 'Subscribe to Advanced Program')    
     }
